00b722379ebe069537225e00c673f686d621a099
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-04-22 20:19:15 +0530

    Something as usual

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { YSocketIO } from "y-socket.io/dist/server";
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer , {
+    cors : {
+        origin : "*",
+        methods : ["GET" , "POST"]
+    }
+})
+
+const ySocketIO = new YSocketIO(io);
+ySocketIO.initialize()
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!!", Success: true });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Ok", Success: true });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running at the port 3000");
+});
